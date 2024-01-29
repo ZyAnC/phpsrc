520f3efa3b41f5c39e26d37e15d6bdcfcd11b073
ZyAnC <yanzheng1214@outlook.com>
2024-01-30 00:43:06 +0200

    s

---
+++ src/ex1.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Exercise 1: Getting Started with PHP - YourFirstName</title>
+</head>
+<body>
+
+<h3>PHP Introduction</h3>
+
+<?php
+// Outputting a simple message
+echo 'Hello world! My name is "David"<br>';
+
+// Using a variable
+$title = "PHP is interesting.";
+echo "<h4>$title</h4>";
+
+// Defining and displaying variables in a table
+$g1 = 5;
+$g2 = 4;
+$g3 = 5;
+
+echo "<table border='1'>
+        <tr>
+            <th>S.n.</th>
+            <th>Name</th>
+            <th>Grade</th>
+        </tr>
+        <tr>
+            <td>1</td>
+            <td>John</td>
+            <td>$g1</td>
+        </tr>
+        <tr>
+            <td>2</td>
+            <td>Alice</td>
+            <td>$g2</td>
+        </tr>
+        <tr>
+            <td>3</td>
+            <td>Bob</td>
+            <td>$g3</td>
+        </tr>
+      </table>";
+
+// Insert your screenshot here. You can use an <img> tag to include it.
+// Example: echo '<img src="path_to_your_screenshot.jpg" alt="Development Environment Screenshot">';
+
+?>
+
+</body>
+</html>
+
