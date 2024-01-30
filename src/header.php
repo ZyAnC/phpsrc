13215a35e1073e95210ed242970d400879f92176
Yan Zheng <yzheng@c2r6p6.hive.fi>
2024-01-30 17:30:55 +0200

    2

---
+++ src/header.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Your Website Title</title>
+    <!-- Add your CSS, JavaScript, etc. here -->
+</head>
+<body>
+    <header>
+        <!-- Your header content goes here -->
+    </header>
